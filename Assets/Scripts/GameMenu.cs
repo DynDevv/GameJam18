058d6fdd7cc621fb7bd32714d52cb19b23ba2baf
Marlene Mayr <Marlene-Mayr@live.com>
2018-12-08 19:20:19 +0100

    UI
    - added Playerscript
    - added Gaegu fonts
    - added menus
    - added full menu scene

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    //public AudioManager audioManager;
+    public GameManager gameManager;
+
+    public void PauseGame()
+    {
+        gameManager.ChangePauseState(true);
+    }
+
+    public void ResumeGame()
+    {
+        gameManager.ChangePauseState(false);
+    }
+
+    public void ChangeMusicVolume(Slider slider)
+    {
+        Debug.Log("Music volume: " + slider.value);
+        //audioManager.
+    }
+
+    public void ChangeAmbientVolume(Slider slider)
+    {
+        Debug.Log("Ambient Sounds volume: " + slider.value);
+    }
+
+    public void ChangeSFXVolume(Slider slider)
+    {
+        Debug.Log("Sound Effects volume: " + slider.value);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
