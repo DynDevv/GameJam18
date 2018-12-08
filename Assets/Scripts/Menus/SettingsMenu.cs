4fd4cf7b44235c803b2921393a3cfab01a726f8f
tester3000 <wanderfalken@gmx.at>
2018-12-08 23:44:44 +0100

    Fixed errors

--- Assets/Scripts/Menus/SettingsMenu.cs
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -73,7 +73,6 @@ public class SettingsMenu : MonoBehaviour {
         {
             b.interactable = toggle.isOn;
         }
-<<<<<<< HEAD
 
         ToggleImageVisibility(toggle.isOn);
     }
@@ -90,8 +89,6 @@ public class SettingsMenu : MonoBehaviour {
             tempColor.a = 0.75f;
         }
         image.color = tempColor;
-=======
->>>>>>> d5361c4c5f1b1ff07a9fe1fda2e8643e00409d96
     }
 
     public void AdjustTimeSlider(GameObject Time)
