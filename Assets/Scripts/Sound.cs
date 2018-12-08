c45091da3abcbc1c097c77d75a9edc69f319ecd9
meisterhauns <hauns97@gmail.com>
2018-12-08 17:23:00 +0100

    Audio Update

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound {
+
+    public string name;
+    public AudioClip audio;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
