a01509811839529b423d32953993f6e90dbd3a70
tester3000 <wanderfalken@gmx.at>
2018-12-09 19:31:08 +0100

    Fixed warning

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -14,22 +14,9 @@ public class AudioManager : MonoBehaviour {
 
     public Sound[] sounds;
 
-    public static AudioManager instance;
 
-	void Awake () {
-
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        DontDestroyOnLoad(gameObject);
-
+	void Awake ()
+    {
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
