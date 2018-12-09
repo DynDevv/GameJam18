1b0379cecdb35117b978fc72483238b6998837d9
meisterhauns <hauns97@gmail.com>
2018-12-09 15:53:21 +0100

    AudioButtons

--- Assets/Scripts/Audio/AudioEnabler.cs
+++ Assets/Scripts/Audio/AudioEnabler.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioEnabler : MonoBehaviour
 {
 
-    // Use this for initialization
+    public GameObject[] buttons;
+    public GameObject backbutton;
+
     void Start()
     {
         FindObjectOfType<AudioManager>().Play("Ambient");
         FindObjectOfType<AudioManager>().Play("Music");
+    }
+
+    public void findButtons()
+    {
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+        backbutton = GameObject.Find("backButton");
+        Debug.LogError(backbutton);
+
+        foreach (GameObject b in buttons)
+        {
+            b.GetComponent<Button>().GetComponent<Button>().onClick.AddListener(PlayButtonSound);
+            Debug.LogError(b);
+        }
+
+        backbutton.GetComponent<Button>().GetComponent<Button>().onClick.AddListener(PlayBackSound);
+    }
+
+    void PlayButtonSound()
+    {
+        FindObjectOfType<AudioManager>().Play("Normal Button");
+    }
+
+    void PlayBackSound()
+    {
+        FindObjectOfType<AudioManager>().Play("Back Button");
     }
 }
