d4d34b1cc7498a34657683b87c44238ee5bc8d79
meisterhauns <hauns97@gmail.com>
2018-12-09 19:13:19 +0100

    Fixed Audio Enabler

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -14,7 +14,21 @@ public class AudioManager : MonoBehaviour {
 
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
 	void Awake () {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
 
         foreach(Sound s in sounds)
         {
