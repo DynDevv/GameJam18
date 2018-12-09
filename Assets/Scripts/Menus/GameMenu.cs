33b92af6e072be2e7ff76bbfc1560c550a0be6cf
Marlene Mayr <Marlene-Mayr@live.com>
2018-12-09 13:27:30 +0100

    resultsscreen incl prefab and better UI control ingame
    also weird hat png activities. guess it's a merge

--- Assets/Scripts/Menus/GameMenu.cs
+++ Assets/Scripts/Menus/GameMenu.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameMenu : MonoBehaviour {
 
     public AudioMixer audioMixer;
     public GameManager gameManager;
+    public GameObject ResultPrefab;
 
     void Start()
     {
@@ -56,6 +58,40 @@ public class GameMenu : MonoBehaviour {
         }
         audioMixer.EditSlider(2, slider.value);
         //Debug.Log("Sound Effects volume: " + slider.value);
+    }
+
+    public void SetCountdownActive(bool state)
+    {
+        gameObject.transform.Find("Countdown").gameObject.SetActive(state);
+    }
+
+    public void SetCountdownText(string value)
+    {
+        gameObject.transform.Find("Countdown").GetComponentInChildren<TextMeshProUGUI>().SetText(value);
+    }
+
+    public void ShowResults(List<SpawnArea> activeDogs)
+    {
+        gameObject.transform.Find("pauseButton").gameObject.SetActive(false);
+        gameObject.transform.Find("ResultsMenu").gameObject.SetActive(true);
+
+        // TODO sort
+
+        gameObject.transform.Find("players");
+        float height = ResultPrefab.transform.position.y;
+        foreach (SpawnArea spawn in activeDogs)
+        {
+            // spawn at correct height
+            Vector3 pos = new Vector3(ResultPrefab.transform.position.x, height, ResultPrefab.transform.position.z);
+            GameObject score = Instantiate(ResultPrefab, pos, ResultPrefab.transform.rotation);
+            score.transform.parent = gameObject.transform.Find("players").transform;
+            height -= 100;
+
+            // set rank, icon, name and score
+            //spawn.GetOwner().GetComponent<Dog>().GetPlayer();
+            //spawn.GetSheep();
+            //output e.g. 1. 1. 3.
+        }
     }
 
     public void RestartGame()
